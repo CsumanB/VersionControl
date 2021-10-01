b3888bbae384c79098f3b49cc69afe6d9e6a036b
csumi <csumi@DESKTOP-FRC4KHI>
2021-10-01 20:42:27 +0200

    7. f

--- Usermaintenance/Usermaintenance/Properties/Form1.cs
+++ Usermaintenance/Usermaintenance/Properties/Form1.cs
@@ -21,8 +21,7 @@ namespace Usermaintenance
             listUsers.DataSource = users;
             listUsers.ValueMember = "ID";
             listUsers.DisplayMember = "FullName";
-            lblLastName.Text = Resource1.LastName; // label1
-            lblFirstName.Text = Resource1.FirstName; // label2
+            lblFullName.Text = Resource1.FullName; // label1
             btnAdd.Text = Resource1.Add; // button1
         }
 
@@ -30,8 +29,8 @@ namespace Usermaintenance
         {
             var u = new User()
             {
-                LastName = txtLastName.Text,
-                FirstName = txtFirstName.Text
+               FullName = txtFullName.Text,
+               
             };
             users.Add(u);
         }
