579ae22892938089131cf2d8cb4b693f4dc45617
csumi <csumi@DESKTOP-FRC4KHI>
2021-10-01 21:43:14 +0200

    9

--- Usermaintenance/Usermaintenance/Properties/Form1.cs
+++ Usermaintenance/Usermaintenance/Properties/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,6 +34,23 @@ namespace Usermaintenance
                
             };
             users.Add(u);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                foreach (var s in users)
+                {
+                    sw.Write(s.ID);
+                    sw.Write(";");
+                    sw.Write(s.FullName);
+                    sw.WriteLine();
+
+                }
+            }
         }
     }
 }
