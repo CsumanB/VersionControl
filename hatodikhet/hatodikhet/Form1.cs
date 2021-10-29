daf72fb1d5fc4be0b61c6c1327446e99df6174a1
csumi <csumi@DESKTOP-FRC4KHI>
2021-10-29 22:18:57 +0200

    hetedikfeladat

--- hatodikhet/hatodikhet/Form1.cs
+++ hatodikhet/hatodikhet/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace hatodikhet
@@ -20,6 +21,7 @@ namespace hatodikhet
         {
             InitializeComponent();
             GetExchangeRates();
+            CreateChart();
             dataGridView1.DataSource = Rates;
         }
 
@@ -61,6 +63,24 @@ namespace hatodikhet
                     rate.Value = value / unit;
             }
 
+        }
+        private void CreateChart()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
         private void Form1_Load(object sender, EventArgs e)
         {
