a16c9bcb7544ca82888181fb34f34163de32e2be
csumi <csumi@DESKTOP-FRC4KHI>
2021-11-12 21:47:11 +0100

    szinvalaszto

--- WindowsFormsApp7/WindowsFormsApp7/Form1.Designer.cs
+++ WindowsFormsApp7/WindowsFormsApp7/Form1.Designer.cs
@@ -36,7 +36,7 @@ namespace WindowsFormsApp7
             this.lblNext = new System.Windows.Forms.Label();
             this.btnSelectCar = new System.Windows.Forms.Button();
             this.btnSelectBall = new System.Windows.Forms.Button();
-            this.mainPanel.SuspendLayout();
+            this.btnColor = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -44,12 +44,9 @@ namespace WindowsFormsApp7
             this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.mainPanel.Controls.Add(this.btnSelectBall);
-            this.mainPanel.Controls.Add(this.btnSelectCar);
-            this.mainPanel.Controls.Add(this.lblNext);
-            this.mainPanel.Location = new System.Drawing.Point(12, 12);
+            this.mainPanel.Location = new System.Drawing.Point(12, 104);
             this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(776, 435);
+            this.mainPanel.Size = new System.Drawing.Size(776, 343);
             this.mainPanel.TabIndex = 0;
             // 
             // createTimer
@@ -67,15 +64,16 @@ namespace WindowsFormsApp7
             // lblNext
             // 
             this.lblNext.AutoSize = true;
-            this.lblNext.Location = new System.Drawing.Point(326, 19);
+            this.lblNext.Location = new System.Drawing.Point(327, 9);
             this.lblNext.Name = "lblNext";
             this.lblNext.Size = new System.Drawing.Size(68, 13);
             this.lblNext.TabIndex = 0;
             this.lblNext.Text = "Coming next:";
+            this.lblNext.Click += new System.EventHandler(this.lblNext_Click);
             // 
             // btnSelectCar
             // 
-            this.btnSelectCar.Location = new System.Drawing.Point(29, 19);
+            this.btnSelectCar.Location = new System.Drawing.Point(31, 26);
             this.btnSelectCar.Name = "btnSelectCar";
             this.btnSelectCar.Size = new System.Drawing.Size(75, 23);
             this.btnSelectCar.TabIndex = 1;
@@ -85,7 +83,7 @@ namespace WindowsFormsApp7
             // 
             // btnSelectBall
             // 
-            this.btnSelectBall.Location = new System.Drawing.Point(147, 18);
+            this.btnSelectBall.Location = new System.Drawing.Point(129, 26);
             this.btnSelectBall.Name = "btnSelectBall";
             this.btnSelectBall.Size = new System.Drawing.Size(75, 23);
             this.btnSelectBall.TabIndex = 2;
@@ -93,18 +91,31 @@ namespace WindowsFormsApp7
             this.btnSelectBall.UseVisualStyleBackColor = true;
             this.btnSelectBall.Click += new System.EventHandler(this.btnSelectBall_Click);
             // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.Color.Fuchsia;
+            this.btnColor.Location = new System.Drawing.Point(224, 26);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(75, 23);
+            this.btnColor.TabIndex = 3;
+            this.btnColor.UseVisualStyleBackColor = false;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.lblNext);
+            this.Controls.Add(this.btnSelectBall);
             this.Controls.Add(this.mainPanel);
+            this.Controls.Add(this.btnSelectCar);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.mainPanel.ResumeLayout(false);
-            this.mainPanel.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -116,6 +127,7 @@ namespace WindowsFormsApp7
         private System.Windows.Forms.Button btnSelectBall;
         private System.Windows.Forms.Button btnSelectCar;
         private System.Windows.Forms.Label lblNext;
+        private System.Windows.Forms.Button btnColor;
     }
 }
 
