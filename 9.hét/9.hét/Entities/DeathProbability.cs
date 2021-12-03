f3f7e4e96be518ada7c23fb9771f1f228532630d
csumi <csumi@DESKTOP-FRC4KHI>
2021-12-03 19:32:59 +0100

    osztalyok létrehozása

---
+++ 9.hét/9.hét/Entities/DeathProbability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.hét.Entities
+{
+    public class DeathProbability
+    {
+        public int Age { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public double P { get; set; }
+    }
+}
---
+++ 9.hét/9.hét/Entities/Gender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.hét.Entities
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+
+}
+
