6823f017510c4d2a418e866bad5688fddf675ac8
csumi <csumi@DESKTOP-FRC4KHI>
2021-12-10 22:36:39 +0100

    új generáció

--- week10/week10/Form1.cs
+++ week10/week10/Form1.cs
@@ -49,6 +49,23 @@ namespace week10
                              select p;
             var topPerformers = playerList.Take(populationSize / 2).ToList();
 
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var brain = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(brain.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(brain);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(brain.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(brain.Mutate());
+            }
+            gc.Start();
+
+
         }
     }
 }
